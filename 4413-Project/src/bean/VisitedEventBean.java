1d419f487496082a8eb7463eab634edeb9de4c65
antep <antep@DESKTOP-F85563I>
2019-02-24 17:10:44 -0500

    finished visit event DAO

--- 4413-Project/src/bean/VisitedEventBean.java
+++ 4413-Project/src/bean/VisitedEventBean.java
@@ -2,5 +2,38 @@ package bean;
 
 public class VisitedEventBean {
 
-		
+	private String[] eventTypes = {"VIEW", "CART", "PURCHASE"};
+	private String day;
+	private String bid;
+	private String eventType;
+	
+	public VisitedEventBean(String day, String bid, String eventType) {
+		this.day = day;
+		this.bid = bid;
+		this.eventType = eventType;
+	}
+	
+	public String getDay() {
+		return day;
+	}
+	
+	public String getBid() {
+		return bid;
+	}
+	
+	public String getEventType() {
+		return eventType;
+	}
+	
+	public void setDay(String day) {
+		this.day = day;
+	}
+	
+	public void setBid(String bid) {
+		this.bid = bid;
+	}
+	
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
 }
--- 4413-Project/src/dao/VisitedEventDAO.java
+++ 4413-Project/src/dao/VisitedEventDAO.java
@@ -1,10 +1,25 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import bean.VisitedEventBean;
+
 public class VisitedEventDAO {
+	
+	// DB table definition
+	private static final String TABLE_NAME = "VisitedEvent";
+	private static final String COL_DAY = "day";
+	private static final String COL_BID = "bid";
+	private static final String COL_TYPE = "eventtype";
 
 	private DataSource ds;
 	
@@ -15,5 +30,50 @@ public class VisitedEventDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public Map<String, VisitedEventBean> retrieve() throws SQLException{
+		
+		String query = "select * from "+TABLE_NAME;
+		Connection conn = ds.getConnection();
+		
+		PreparedStatement stmtObj = conn.prepareStatement(query);
+		ResultSet rs = stmtObj.executeQuery();
+		
+		Map<String, VisitedEventBean> result = new HashMap<String, VisitedEventBean>();
+		
+		while(rs.next()) {
+			String day = rs.getString(COL_DAY);
+			String bid = rs.getString(COL_BID);
+			String type = rs.getString(COL_TYPE);
+			
+			VisitedEventBean bean = new VisitedEventBean(day, bid, type);
+			
+			result.put(bid, bean);
+		}
+		
+		rs.close();
+		stmtObj.close();
+		conn.close();
+		
+		return result;
+	}
+	
+	public void insert(VisitedEventBean bean) throws SQLException {
+		
+		String query = "insert into "+TABLE_NAME+" values(?,?,?)";
+		Connection conn = ds.getConnection();
+		PreparedStatement stmtObj = conn.prepareStatement(query);
+		
+		stmtObj.setString(1, bean.getDay());
+		stmtObj.setString(2, bean.getBid());
+		stmtObj.setString(3, bean.getEventType());
+		
+		stmtObj.executeUpdate();
+		conn.commit();
+		conn.rollback(); 
+		
+		stmtObj.close();
+		conn.close();
 	}
 }
